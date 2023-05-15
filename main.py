4597b9545e71009c7867d6d0c395348c37bdda75
Slep-wt <48381358+Slep-wt@users.noreply.github.com>
2023-05-15 19:52:58 +1000

    Change to JSON
    head hurt but it works

--- main.py
+++ main.py
@@ -1,38 +1,78 @@
+from collections import defaultdict
 import tabula
 import PyPDF2 as pdf2
 import re
+import json
+
+##########################
+# Filename: main.py
+# Project: Route Decoder
+# Purpose: Collates Airservices Australia ERSA GEN FPR routes into a software friendly format
+# Author: Rakshan Chandu
+##########################
 
 
-fpr_domestic = '9. FLIGHT PLANNING OPTIONS'
+
+fprDomestic = '9. FLIGHT PLANNING OPTIONS'
 
 lookupStart = 0
 
 with open('FPR_23MAR2023.pdf','rb') as f:
     reader = pdf2.PdfReader(f)
     for pn in range(len(reader.pages)):
-        pagetext = reader.pages[pn].extract_text()
-        #if (pagetext cont)
-        if re.search(fpr_domestic, pagetext):
+        pageText = reader.pages[pn].extract_text()
+        if re.search(fprDomestic, pageText):
             lookupStart = pn
             break
-    tabula.convert_into("https://www.airservicesaustralia.com/aip/current/ersa/GUID_ersa-fac-2-2_23MAR2023.pdf","latest_routes.csv", output_format='csv', lattice=True, stream=False, pages=[str(lookupStart+1) + '-' + str(len(reader.pages))])
+    tabula.convert_into("https://www.airservicesaustralia.com/aip/current/ersa/GUID_ersa-fac-2-2_23MAR2023.pdf","latest_routes.json", output_format='json', lattice=True, stream=False, pages=[str(lookupStart+1) + '-' + str(len(reader.pages))])
 
-csvdat = ''
-with open('latest_routes.csv', 'r') as f:
-    csvdat = f.read().replace('%','Jet Only').replace('#','').replace('>', 'Non-Jet').replace('@','Jet Only (Mil)')
-    
+dat = ''
+jmod = {'data':[]}
+with open('latest_routes.json', 'r') as f:
+    dat = f.read()
 
-with open('latest_routes.csv', 'w') as f:
-    csvdat = re.sub(r'\([R-r]efer GEN-FPR(,|\n|\s)(\n|\s|)para \d\)', '', csvdat).replace('(refer requirements)', '')
-    f.write(csvdat)
- 
-#with open('FPR_23MAR2023.pdf','rb') as f:
-#    reader = pdf2.PdfReader(f)
-#    start = False
-#    for pn in range(len(reader.pages)):
-#        pagetext = reader.pages[pn].extract_text()
-#        #if (pagetext cont)
-#        if re.search(fpr_domestic, pagetext):
-#            start = True
-#        if start:
-#            fw.write(pagetext)
+with open('latest_routes.json', 'w') as f:
+    dat = dat.replace('%','Jet Only').replace('#','').replace('\\u003e', 'Non-Jet').replace('@','Jet Only (Mil)').replace('\\r',' ')
+    dat = re.sub(r'"(top|left|right|width|height|bottom)":(\d+\.?\d*),', '', dat) # remove useless data
+    dat = re.sub(r'"(extraction_method)":"lattice",', '', dat) # remove more junk
+    dat = re.sub(r'\]\},\{"data":\[\[\{"text":""\}\]\]\},\{"data":\[', ',', dat) # reformatting mistakes made by the parser
+    dat = re.sub(r'\]\},\{"data":\[', ',', dat) # more reformatting
+    dat = re.sub(r'\[\{"text":""\}\],', '', dat) # and some more
+    jorg = json.loads(dat)
+
+    pdep = ''
+    pdes = ''
+
+    noteFinder = re.compile(r'\s\([A-Z].*\)')
+
+    for routedata in jorg[0]['data']: # rebuild the json from the ground up (so its not fucked)
+        if (5 <= len(routedata) <= 6):
+            jroute = defaultdict(dict)
+            dep = str(routedata[0]['text'])
+            des = str(routedata[1]['text'])
+            notes = str(routedata[2]['text'])
+            route = str(routedata[4]['text'])
+
+            hasMatch = noteFinder.search(route) # check for any route notes
+            if hasMatch:
+                route = noteFinder.sub('', route)
+                if notes != '':
+                    notes += hasMatch.group(0)
+                else:
+                    notes = hasMatch.group(0)[1:]
+
+            if dep == '': # is this an alternate approved route?
+                dep = pdep
+                des = pdes
+
+            jroute['dep'] = dep
+            jroute['des'] = des
+            jroute['route'] = route
+            jroute['notes'] = notes
+
+            pdep = dep
+            pdes = des
+            jmod['data'].append(jroute)
+
+    dat = json.dumps(jmod)
+    f.write(dat)
